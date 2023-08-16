0aedba36f86bc74a7b0381832de02a78af8446c5
rshulabs <2505269010@qq.com>
2023-08-16 10:22:58 +0000

    feat: 增加跨域中间件

--- internal/cache/controller/router.go
+++ internal/cache/controller/router.go
@@ -2,10 +2,11 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	middleware "github.com/rshulabs/HgCache/internal/pkg/middlewares"
 )
 
 func installHttpRouter(g *gin.Engine) *gin.Engine {
-	api := g.Group("/cache")
+	api := g.Group("/cache").Use(middleware.Cors())
 	{
 		api.GET("/api", Query)
 	}
